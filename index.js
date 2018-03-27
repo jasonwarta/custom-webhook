37b79512bf049c6a6b4576c94748ada24037bc8c
Jason Warta <jasonwarta@gmail.com>
2018-03-27 13:24:55 -0800

    added POST route

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ let express = require('express');
 let app = express();
 
 app.get('*', (req,res) => {
+	console.log('GET')
+	console.log(req,res);
+});
+
+app.post('*', (req,res) => {
+	console.log('POST')
 	console.log(req,res);
 });
 
