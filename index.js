dc9d6652094544ac8ff50d06c2bc109b998c7f86
Jason Warta <jasonwarta@gmail.com>
2018-03-27 13:55:22 -0800

    added some handling

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ let app = express();
 app.use( express.json() );
 
 app.post('/', (req,res) => {
-	console.log(req.body);
+	console.log('push event was triggered');
+	exec("git log --pretty=format:'%H' -n 1", (err, stdout, stderr) => {
+		if (err) {
+			console.log("error");
+			return;
+		}
+		console.log(stdout);
+	});
+	console.log(req.body.after);
 });
 
 app.listen(8888, () => console.log('listening on port 8888'));
